a2ad49125d322e4b9a2ea398153e2ee2a80bfeae
RazmikKerektsyan <88571516+RazmikKerektsyan@users.noreply.github.com>
2026-09-24 22:08:34 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,110 @@
+# name = "Razo"
+# age = 20
+# #Info about person
+# print("Hello " + name + "!")
+# print("How are you " + name + "!")
+# print("How old are you " + name + "!")
+# #print("My age is ", age)
+#
+# print("==="*5)
+# name = "Razmik"
+# age = 25
+# print("Hello " + name + "!")
+# print("My age is ", age)
+
+#name_student_semester1 = "Mst"
+
+#student1 = "giorgi"
+
+#nameStudentSemester2 = "Frsh"
+
+#STUDENT_NAME = "Top"
+a = 17
+b = 5
+# print("Shekreba", a+b)
+# print("gamokleba", a-b)
+# print("gamravla", a * b)
+# print("gayopa", a/b)
+# print("mteli gayopa", a//b)
+# print("nashti", a%b)
+# print("xarisxi", a**3)
+#
+# print(0.1 + 0.2)
+# print(round(0.1 + 0.2, 2))
+
+# ctrl + /  comment all
+
+
+# name = "Razo"# ES ARIS STRINGI (str)
+# age = 20  # ES ARIS INT (int)
+# weight = 75.4 # ES ARIS FLOAT (float)
+# status_is_active = True or False # ES ARIS BOOLEAN (bool)
+# non_variable = None # None type
+# print((name),type(name))
+# print((age), type(age))
+# print((weight), type(weight))
+# print((is_active), type(is_active))
+# print((non_variable), type(non_variable))
+
+# is_active = true  #NameError
+
+# name = input("Type your name: ")
+# print("Hello " + name + "!", type(name))
+# age = int(input("Type your age: "))
+# print("Your age is", age, type(age))
+# height = float(input("Type your height: "))
+# print("Your height is", height, type(height))
+# status = input("Type your status: ")
+# status = bool(status)
+# print("Your status is", status, type(status))
+#  All inputs returns string(str)
+# ete uzum enq vor polory linen irenc type-erov petqe avelacnenq hamapatasxan type
+
+# age = 10
+# print(age)
+# print("====" * 10)
+#
+# age = age + 10
+# print(age)
+#
+# age = age - 5
+# print(age)
+#
+# age = age * 2
+# print(age)
+#
+# age += 10  # es aris erti da igive rac age = age + 10
+# print(age)
+
+# bool_to_int = True
+# bool_to_float = int(bool_to_int)
+# print(bool_to_int, type(bool_to_int))
+#
+# print("====" * 10)
+#
+# bool_to_int = False
+# bool_to_int = int(bool_to_int)
+# print(bool_to_int, type(bool_to_int))
+
+# int_to_bool1 = 0
+# int_to_bool1 = bool(int_to_bool1)
+# print(int_to_bool1, type(int_to_bool1))
+# print("====" * 10)
+#
+# int_to_bool1 = None
+# int_to_bool1 = bool(int_to_bool1)
+# print(int_to_bool1, type(int_to_bool1))
+
+# a = 10
+# b = 2
+# c = a / b
+# print(c, type(c))
+
+
+# a = input("Enter a number: ")
+# b = input("Enter a number: ")
+# print(a+b)
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+print(a+b)
